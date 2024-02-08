7fe115d021becb41ee40db76c5371c3234c54f2c
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-08 18:39:40 -0300

    agregando

--- ejecuta_kfold.sh
+++ ejecuta_kfold.sh
@@ -2,7 +2,7 @@
 
 ipynb-py-convert kfold_validation.ipynb kfold_validation.py
 
-#python3 kfold_validation.py --model 'efficientnet_b3'
+python3 kfold_validation.py --model 'efficientnet_b3'
 python3 kfold_validation.py --model 'inception_resnet_v2'
 python3 kfold_validation.py --model 'inception_v3'
 python3 kfold_validation.py --model 'mobilenet_v3'
