55b4a9f5c74083706dcc56026994d82b8d9f1601
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-02-09 13:05:18 -0300

    ordenando

--- ejecuta_kfold.sh
+++ ejecuta_kfold.sh
@@ -2,10 +2,10 @@
 
 ipynb-py-convert kfold_validation.ipynb kfold_validation.py
 
-python3 kfold_validation.py --model 'efficientnet_b3'
-python3 kfold_validation.py --model 'inception_resnet_v2'
-python3 kfold_validation.py --model 'inception_v3'
-python3 kfold_validation.py --model 'mobilenet_v3'
-python3 kfold_validation.py --model 'resnet_v2_50'
+#python3 kfold_validation.py --model 'efficientnet_b3'
+#python3 kfold_validation.py --model 'inception_resnet_v2'
+#python3 kfold_validation.py --model 'inception_v3'
+python3 kfold_validation.py --model 'mobilenet_v3' --epochs 200 --batch-size 128
+python3 kfold_validation.py --model 'resnet_v2_50' --epochs 200
 
 rm -f kfold_validation.py
