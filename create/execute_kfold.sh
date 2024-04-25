215daaf80219bbfc67ce05c18e82b5e1695bcfef
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-04-24 22:04:18 -0300

    mejorando

---
+++ create/execute_kfold.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PyCommand='#!/usr/bin/python3
+
+json_filename="kfold_data_results.json"
+
+model_list=["efficientnet_b3",
+            "inception_resnet_v2",
+            "inception_v3",
+            "mobilenet_v3",
+            "resnet_v2_50"
+            ];
+
+info_list=[ "mean_val_categorical_accuracy",
+            "std_val_categorical_accuracy",
+            "mean_val_loss",
+            "mean_train_categorical_accuracy",
+            "mean_train_loss"];
+
+sep=",";
+
+image_ext=".eps";
+'
+
+InTrD='/media/fernando/Expansion/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/train'
+InTrF='labels-emotion4-v1.csv'
+DName='perwi'
+OutDir='/media/fernando/Expansion/OUTPUTS/DOCTORADO2/cnn_emotion4_1'
+
+################################################################################
+
+ipynb-py-convert kfold_validation.ipynb kfold_validation.py
+
+python3 kfold_validation.py --model 'efficientnet_b3'     --epochs 75 --batch-size  32 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'inception_v3'        --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 75 --batch-size 128 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+
+cp 'main.py' $OutDir/'main.py'
+echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/'main.py'
+
+rm -f kfold_validation.py
+
