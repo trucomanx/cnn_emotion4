c36b6972e394d6a15bcd0ba730cd8adb46b761f6
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-04-25 13:31:12 -0300

    ordenando

--- create/execute_kfold.sh
+++ create/execute_kfold.sh
@@ -22,10 +22,11 @@ sep=",";
 image_ext=".eps";
 '
 
-InTrD='/media/fernando/Expansion/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/train'
+OutDir='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/OUTPUTS/DOCTORADO2/cnn_emotion4_1'
+
+InTrD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/train'
 InTrF='labels-emotion4-v1.csv'
 DName='perwi'
-OutDir='/media/fernando/Expansion/OUTPUTS/DOCTORADO2/cnn_emotion4_1'
 
 ################################################################################
 
@@ -38,7 +39,7 @@ python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 75 --batch-si
 python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
 
 cp 'main.py' $OutDir/'main.py'
-echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/'main.py'
+echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/'main.py'
 
 rm -f kfold_validation.py
 
