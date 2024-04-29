2aba03325a121789230d14287b184c6bc9392a03
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-04-29 14:42:41 -0300

    ordenando

--- create/execute_training_holdout.sh
+++ create/execute_training_holdout.sh
@@ -45,16 +45,16 @@ fi
 
 ################################################################################
 
-ipynb-py-convert kfold_validation.ipynb kfold_validation.py
+ipynb-py-convert training_holdout.ipynb training_holdout.py
 
-python3 kfold_validation.py --model 'efficientnet_b3'     --epochs 75 --batch-size  32 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'inception_v3'        --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 75 --batch-size 128 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 training_holdout.py --model 'efficientnet_b3'     --epochs 75 --batch-size  32 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 training_holdout.py --model 'inception_resnet_v2' --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 training_holdout.py --model 'inception_v3'        --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 training_holdout.py --model 'mobilenet_v3'        --epochs 75 --batch-size 128 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 training_holdout.py --model 'resnet_v2_50'        --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
 
 cp 'main.py' $OutDir/'main.py'
 echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/'main.py'
 
-rm -f kfold_validation.py
+rm -f training_holdout.py
 
