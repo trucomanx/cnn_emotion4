2fa665625da500ef97c322ee21ccfd9fc43c659e
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-04-29 14:29:55 -0300

    agregando

---
+++ create/execute_training_holdout.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+PyCommand='#!/usr/bin/python3
+
+json_filename="training_data_results.json"
+
+model_list=["efficientnet_b3",
+            "inception_resnet_v2",
+            "inception_v3",
+            "mobilenet_v3",
+            "resnet_v2_50"
+            ];
+
+info_list=[ "train_categorical_accuracy",
+            "val_categorical_accuracy",
+            "test_categorical_accuracy",
+            "train_loss",
+            "val_loss",
+            "test_loss"];
+
+sep=",";
+
+image_ext=".eps";
+'
+
+OutDir='/media/fernando/Expansion/OUTPUTS/DOCTORADO2/cnn_emotion4_1'
+
+DName='perwi' 
+#DName='ber2024-body'
+
+
+if [ "$DName" = "perwi" ]; then
+    InTrD='/media/fernando/Expansion/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/train/'
+    InTrF='labels-emotion4-v1.csv'
+    InTsD='/media/fernando/Expansion/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/test/'
+    InTsF='labels-emotion4-v1.csv'
+fi
+
+if [ "$DName" = "ber2024-body" ]; then
+    InTrD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/DATASET/TESE/BER/BER2024/BER2024-BODY'
+    InTrF='train.csv'
+    InTsD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/DATASET/TESE/BER/BER2024/BER2024-BODY'
+    InTsF='test.csv'
+fi
+
+################################################################################
+
+ipynb-py-convert kfold_validation.ipynb kfold_validation.py
+
+python3 kfold_validation.py --model 'efficientnet_b3'     --epochs 75 --batch-size  32 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'inception_v3'        --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 75 --batch-size 128 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 75 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+
+cp 'main.py' $OutDir/'main.py'
+echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/'main.py'
+
+rm -f kfold_validation.py
+
