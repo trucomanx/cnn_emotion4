ca3be2aae633ccf9efc9e42aa4f2fc20ff5e8a91
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-05-03 13:58:33 -0300

    ordenando

--- create/execute_kfold.sh
+++ create/execute_kfold.sh
@@ -43,13 +43,13 @@ fi
 ipynb-py-convert kfold_validation.ipynb kfold_validation.py
 
 #python3 kfold_validation.py --model 'efficientnet_b3'     --epochs 75 --batch-size  32 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+#python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
 python3 kfold_validation.py --model 'inception_v3'        --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
 python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 75 --batch-size 128 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
 python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 75 --batch-size  64 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
 
 cp 'main.py' $OutDir/'main.py'
-echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/'main.py'
+echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/cross-validation/'main.py'
 
 rm -f kfold_validation.py
 
