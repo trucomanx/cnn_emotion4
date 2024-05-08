352ec8bdbaad8120c5c9926f884903f183ca083f
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-05-08 20:54:44 -0300

    agregando testing

--- create/execute_training_holdout.sh
+++ create/execute_training_holdout.sh
@@ -52,9 +52,9 @@ echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/training_va
 
 ipynb-py-convert training_holdout.ipynb training_holdout.py
 
-python3 training_holdout.py --model 'efficientnet_b3'     --epochs 100 --batch-size  32 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 training_holdout.py --model 'inception_resnet_v2' --epochs 100 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 training_holdout.py --model 'inception_v3'        --epochs 100 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+#python3 training_holdout.py --model 'efficientnet_b3'     --epochs 100 --batch-size  32 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+#python3 training_holdout.py --model 'inception_resnet_v2' --epochs 100 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
+#python3 training_holdout.py --model 'inception_v3'        --epochs 100 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
 python3 training_holdout.py --model 'mobilenet_v3'        --epochs 100 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
 python3 training_holdout.py --model 'resnet_v2_50'        --epochs 100 --batch-size  64 --dataset-train-dir $InTrD --dataset-train-file $InTrF --dataset-test-dir $InTsD --dataset-test-file $InTsF --dataset-name $DName --output-dir $OutDir
 
