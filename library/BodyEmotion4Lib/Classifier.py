af0ecf6504c20724f1bb76eeba9ddeafbb6b050e
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-05-09 17:37:45 -0300

    ordenando

--- library/BodyEmotion4Lib/Classifier.py
+++ library/BodyEmotion4Lib/Classifier.py
@@ -5,33 +5,56 @@ import BodyEmotion4Lib.lib_model as mpp
 import PIL
 
 class Emotion4Classifier:
+    """Class to classify 4 body languages.
+    
+    The class Emotion4Classifier classify daa in 4 body languages.
+    
+    Args:
+        model_type: Type of model architecture. 
+        This can be: 'efficientnet_b3', 'inception_resnet_v2', 'inception_v3', 'mobilenet_v3' and 'resnet_v2_50'
+        
+    Atributos:
+        modelo: Model returned by tensorflow.
+        model_type: Architecture of model.
+        target_size: Input size of model. The input image will be reshape to (target_size,target_size,3).
+    """
     def __init__(self,model_type='efficientnet_b3'):
-        checkpoint_path='';
+        """Inicializer of class Emotion4Classifier.
+        
+        Args:
+            model_type: Type of model architecture. 
+            This can be: 'efficientnet_b3', 'inception_resnet_v2', 'inception_v3', 'mobilenet_v3' and 'resnet_v2_50'
+        """
         self.model_type=model_type;
         
-        if self.model_type=='mobilenet_v3':
-            checkpoint_path=os.path.join('models','model_mobilenet_v3.h5');
-        elif self.model_type=='resnet_v2_50':
-            checkpoint_path=os.path.join('models','model_resnet_v2_50.h5');
-        elif self.model_type=='efficientnet_b3':
-            checkpoint_path=os.path.join('models','model_efficientnet_b3.h5');
-        elif self.model_type=='inception_v3':
-            checkpoint_path=os.path.join('models','model_inception_v3.h5');
-        elif self.model_type=='inception_resnet_v2':
-            checkpoint_path=os.path.join('models','model_inception_resnet_v2.h5');
-        else:
-            raise TypeError("Unknown parameter model_type");
-        
-        path = os.path.dirname(__file__)
-        path = os.path.join(path,checkpoint_path);
-        
-        self.modelo, self.target_size=mpp.create_model(path,model_type=self.model_type);
+        self.modelo, self.target_size=mpp.create_model(model_type=self.model_type,load_weights=True,file_of_weight='');
     
-    def is_file_patient(self,imgfilepath):
+    def from_img_filepath(self,imgfilepath):
+        """Classify a image from the image filepath.
+        
+        Args:
+            imgfilepath: The iamge filepath.
+        
+        Returns:
+            int: The class of image.
+        """
         return mpp.evaluate_model_from_file(self.modelo,imgfilepath, target_size=self.target_size);
 
-    def is_pil_patient(self,img_pil):
+    def from_img_pil(self,img_pil):
+        """Classify a image from a PIL object.
+        
+        Args:
+            img_pil: The PIL object.
+        
+        Returns:
+            int: The class of image.
+        """
         return mpp.evaluate_model_from_pil(self.modelo,img_pil, target_size=self.target_size);
     
     def target_labels(self):
-        return ['Negative','Neutro','Pain','Positive'];
+        """Returns the categories of classifier.
+        
+        Returns:
+            list: The labels of categories resturned by the methods from_img_pil() and from_img_filepath().
+        """
+        return ['negative','neutro','pain','positive'];
