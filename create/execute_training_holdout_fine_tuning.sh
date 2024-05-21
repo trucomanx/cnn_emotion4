28fa13fdd55f843340f7be2335d99d6715b886ed
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-05-21 07:49:34 -0300

    Update execute_training_holdout_fine_tuning.sh

--- create/execute_training_holdout_fine_tuning.sh
+++ create/execute_training_holdout_fine_tuning.sh
@@ -13,7 +13,8 @@ model_list=["efficientnet_b3",
 
 info_list=[ "train_categorical_accuracy",
             "val_categorical_accuracy",
-            "test_categorical_accuracy"
+            "test_categorical_accuracy",
+            "number_of_parameters"
             ];
 
 sep=",";
