9483c2089b35f51172b3029c1b7cd9138b8857a5
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-07-05 15:54:02 -0300

    ordenando

--- create/execute_testing_holdout.sh
+++ create/execute_testing_holdout.sh
@@ -20,21 +20,21 @@ sep=",";
 image_ext=".eps";
 '
 
-OutDir='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/OUTPUTS/DOCTORADO2/cnn_emotion4_1'
+OutDir='/media/fernando/Expansion/OUTPUTS/DOCTORADO2/cnn_emotion4_1'
 
 #DName='perwi'  
 DName='ber2024-body'  
 
 if [ "$DName" = "perwi" ]; then
-    InTsD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/test/'
+    InTsD='/media/fernando/Expansion/DATASET/TESE/PATIENT-RECOGNITION/PATIENT-IMAGES/perwi/dataset/test/'
     InTsF='labels-emotion4-v1.csv'
-    ModD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/OUTPUTS/DOCTORADO2/cnn_emotion4/perwi/training_validation_holdout'
+    ModD='/media/fernando/Expansion/OUTPUTS/DOCTORADO2/cnn_emotion4/perwi/training_validation_holdout_fine_tuning'
 fi
 
 if [ "$DName" = "ber2024-body" ]; then
-    InTsD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/DATASET/TESE/BER/BER2024/BER2024-BODY'
+    InTsD='/media/fernando/Expansion/DATASET/TESE/BER/BER2024/BER2024-BODY'
     InTsF='test.csv'
-    ModD='/media/fernando/B0EA304AEA300EDA/Dados/Fernando/OUTPUTS/DOCTORADO2/cnn_emotion4/ber2024-body/training_validation_holdout'
+    ModD='/media/fernando/Expansion/OUTPUTS/DOCTORADO2/cnn_emotion4/ber2024-body/training_validation_holdout_fine_tuning'
 fi
 
 ################################################################################
@@ -43,14 +43,15 @@ mkdir -p $OutDir/$DName/test_holdout
 echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/test_holdout/'main.py'
 
 ################################################################################
+export TF_USE_LEGACY_KERAS=1 
 
 ipynb-py-convert testing_holdout.ipynb testing_holdout.py
 
-python3 testing_holdout.py --model 'efficientnet_b3'     --model-dir $ModD/'efficientnet_b3'     --times 10 --batch-size  32 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 testing_holdout.py --model 'inception_resnet_v2' --model-dir $ModD/'inception_resnet_v2' --times 10 --batch-size  64 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 testing_holdout.py --model 'inception_v3'        --model-dir $ModD/'inception_v3'        --times 10 --batch-size  64 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 testing_holdout.py --model 'mobilenet_v3'        --model-dir $ModD/'mobilenet_v3'        --times 10 --batch-size  64 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
-python3 testing_holdout.py --model 'resnet_v2_50'        --model-dir $ModD/'resnet_v2_50'        --times 10 --batch-size  64 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 testing_holdout.py --model 'efficientnet_b3'     --model-file $ModD/'efficientnet_b3/model_efficientnet_b3.h5'         --times 1 --batch-size  16 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 testing_holdout.py --model 'inception_resnet_v2' --model-file $ModD/'inception_resnet_v2/model_inception_resnet_v2.h5' --times 1 --batch-size  16 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 testing_holdout.py --model 'inception_v3'        --model-file $ModD/'inception_v3/model_inception_v3.h5'               --times 1 --batch-size  16 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 testing_holdout.py --model 'mobilenet_v3'        --model-file $ModD/'mobilenet_v3/model_mobilenet_v3.h5'               --times 1 --batch-size  16 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
+python3 testing_holdout.py --model 'resnet_v2_50'        --model-file $ModD/'resnet_v2_50/model_resnet_v2_50.h5'               --times 1 --batch-size  16 --dataset-dir $InTsD --dataset-file $InTsF --dataset-name $DName --output-dir $OutDir
 
 rm -f testing_holdout.py
 
